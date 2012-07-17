d1ee9bf44eb04ca15683479afabb63bc758de279
Liao YuLei <dualface@gmail.com>
2012-07-17 10:11:24 +0800

    add REQUEST_METHOD global var

--- framework/init.lua
+++ framework/init.lua
@@ -1,8 +1,9 @@
 
 require("framework.functions")
 
+REQUEST_METHOD = ngx.req.get_method()
 GET = ngx.req.get_uri_args()
-if ngx.req.get_method() == "POST" then
+if REQUEST_METHOD == "POST" then
     POST = ngx.req.get_post_args()
 else
     POST = {}
